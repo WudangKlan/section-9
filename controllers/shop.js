97ae445ec4116b1e360502b90c2b6c819f94b9f4
Henry.Yeo <Henry.Yeo@easecoxgroup.com>
2023-04-11 17:52:43 +0700

    extracting params l-119

---
+++ controllers/shop.js
@@ -0,0 +1,59 @@
+const Product = require("../models/product");
+
+exports.getIndex = (req,res,next)=>{
+  Product.fetchAll((products) => {
+    res.render("shop/index", {
+      prods: products,
+      pageTitle: "Shop",
+      path: "/",
+    });
+  });
+}
+
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render("shop/product-list", {
+      prods: products,
+      pageTitle: "product list",
+      path: "/products",
+    });
+  });
+};
+
+exports.getProduct = (req,res,next)=>{
+  const prodId = req.params.productId;
+  Product.findById(prodId,(product)=>console.log(product))
+  res.redirect('/')
+}
+
+exports.getCart = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render("shop/cart", {
+      prods: products,
+      pageTitle: "Cart",
+      path: "/cart",
+    });
+  });
+};
+
+exports.getOrders = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render("shop/orders", {
+      prods: products,
+      pageTitle: "Your Orders",
+      path: "/orders",
+    });
+  });
+};
+
+
+exports.getCheckout = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render("shop/checkout", {
+      prods: products,
+      pageTitle: "Checkout",
+      path: "/checkout",
+    });
+  });
+};
